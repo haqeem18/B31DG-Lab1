86365fb564ffb499c951c4260d9993f10d242764
haqeem18 <67266213+haqeem18@users.noreply.github.com>
2023-02-16 22:53:27 +0000

    Update mode 2 inverse
    Added comments to understand the code

--- Assignment_1.ino
+++ Assignment_1.ino
@@ -6,7 +6,7 @@
 
 // Signal A parameter
 int param_a = 100;  // initial width
-int param_b = 600;  // between pulses
+int param_b = 700;  // between pulses
 int param_c = 6;    // no. of pulses
 int param_d = 4500; // end delayMicroseconds of waveform
 
@@ -19,51 +19,56 @@ void setup()
     Serial.begin(115200);
 
     // Set signal output pin
-    pinMode(SigA_pin, OUTPUT);
-    pinMode(SigB_pin, OUTPUT);
-    pinMode(ButtonA_pin, INPUT);
-    pinMode(ButtonB_pin, INPUT); 
+    pinMode(SigA_pin, OUTPUT);	// Set SigA_pin as output
+    pinMode(SigB_pin, OUTPUT);	// Set SigB_pin as output
+    pinMode(ButtonA_pin, INPUT);	// Set ButtonA_pin as input
+    pinMode(ButtonB_pin, INPUT); 	// Set ButtonB_pin as input
 }
 
 void loop()
 {
+    // Execute waveform when Button A = LOW
     if (digitalRead(ButtonA_pin) == LOW)
     {
-      pulse(SigB_pin, paramB_high, 0);
-      SigA_output(param_a, param_b, param_c, param_d);
+      pulse(SigB_pin, paramB_high, 0);			// Generate Signal B
+      SigA_output(param_a, param_b, param_c, param_d);	// Generate Signal A
+
     }
 }
 
+// Generate Signal A waveform
 void SigA_output(int a, int b, int c, int d)
 {
     int add = 0;
     
+    // Signal A (normal)
     if (digitalRead(ButtonB_pin) == LOW)
     {
-        for (int i=0; i<c; i++)
+        for (int i=0; i<c; i++)		// Repeat loop for c amount of cycle
         {
-            pulse(SigA_pin, a + add, b);
-            add = add + 50;
+            pulse(SigA_pin, a + add, b); 	// Generate pulse for 1st,2nd,3rd,etc
+            add = add + 50;		   	// Increment pulse width by 50 us
         }
         delayMicroseconds(d);
     }
+    // Signal A (inverted)
     else
     {
-        a = a + (c-1)*50;
-        for (int i=0; i<c; i++)
+        a = a + (c-1)*50;		  	// Calculate the longest pulse width
+        for (int i=0; i<c; i++)		// Repeat loop for c amount of cycle
         {
-            pulse(SigA_pin, a + add, b);
-            add = add - 50;
+            pulse(SigA_pin, a + add, b); 	// Generate pulse for 1st,2nd,3rd,etc
+            add = add - 50;	           	// Decrement pulse width by 50 us
         }
         delayMicroseconds(d);
     }
 }
 
-
+// Generate one pulse
 void pulse(int pin, int high_width, int low_width)
 {
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(high_width);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(low_width);
+  digitalWrite(pin, HIGH);			// Set output pin to HIGH
+  delayMicroseconds(high_width);		// Width during HIGH 
+  digitalWrite(pin, LOW);			// Set output pin to LOW
+  delayMicroseconds(low_width);		// Width for LOW
 }
